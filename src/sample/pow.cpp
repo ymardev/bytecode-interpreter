f747715333edb0a9d127bef3989b8f21e884ae96
ymardev <82160740+ymardev@users.noreply.github.com>
2021-02-09 15:28:40 +0100

    feat - add special case and comments to 'pow' bytecode program

--- src/sample/pow.cpp
+++ src/sample/pow.cpp
@@ -12,14 +12,16 @@ std::vector<Instruction> pow_program(Nat n, Nat exp)
     return
     {
         set(rN, n),         // Save original value.
-        set(rACC, n),       // Value will accumulate in here.
+        eq(rN, 0),          // Special case, n is 0.
+        ret(0),
         set(rEXP, exp),
-        eq(rEXP, 0),        // Special case: check for exponent = 0.
+        eq(rEXP, 0),        // Special case, exponent is 0.
         ret(1),
+        set(rACC, n),       // Value will accumulate in here.
         eq(rEXP, 1),        // When exponent is 1,
-        ret(rACC),          // return accumulator value;
-        mul(rACC, rN),      // otherwise do a multiplication
+        ret(rACC),          // return accumulator value.
+        mul(rACC, rN),      // Otherwise do a multiplication
         add(rEXP, -1),      // and decrease exponent (loop counter).
-        jmp(-4)
+        jmp(-4)             // Then start again.
     };
 }
