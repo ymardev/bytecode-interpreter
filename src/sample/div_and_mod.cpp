13f689b6bff4590ffb74dfcf2ede9ac34ad21049
ymardev <82160740+ymardev@users.noreply.github.com>
2021-02-11 21:06:39 +0100

    fix  - add 0 handling in div_and_mod sample program

--- src/sample/div_and_mod.cpp
+++ src/sample/div_and_mod.cpp
@@ -10,10 +10,18 @@ std::vector<Instruction> div_and_mod_program(Nat a, Nat b)
 
     return
     {
-        set(R0, a),
+        set(R0, b),
+        eq(R0, 0),      // trying to divide by 0
+        jmp(7),
+        set(R0, a),     // trying to divide 0
+        eq(R0, 0),
+        jmp(4),
         set(R1, a),
         div(R0, b),
         mod(R1, b),
+        jmp(2),
+        set(R0, 0),     // handle the case where one or both args are 0
+        set(R1, 0),
         ret(0)
     };
 }
